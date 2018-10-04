972b0548cba71ed407b4dc5019397030b3c87722
Axolotl <samuelmachat@gmail.com>
2018-10-04 18:14:02 -0400

    added intro button and answer buttons

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -6,12 +6,30 @@ import './index.css'
 
 // import components
 import Header from './components/Header'
+import IntroButton from './components/IntroButton'
+import Questions from './components/Questions'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      started: false
+    }
+  }
+
+  startQuestions = () => {
+    this.setState({ started: true })
+  }
+
   render() {
+    const { started } = this.state
+    const { startQuestions } = this
+
     return (
       <div className="container">
         <Header />
+        <IntroButton startQuestions={startQuestions} />
+        {started && <Questions />}
       </div>
     )
   }
