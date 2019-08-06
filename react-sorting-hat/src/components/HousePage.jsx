50ad5ec16c1ac4a8ac9ecd162041d9ef22558234
Eli Sacks <bastlifa@gmail.com>
2019-08-06 19:30:21 -0400

    initial components and such

---
+++ react-sorting-hat/src/components/HousePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HousePage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default HousePage
