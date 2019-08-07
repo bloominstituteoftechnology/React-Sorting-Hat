6bf73a39175d7b76761c294d36755ee535309321
Eli Sacks <bastlifa@gmail.com>
2019-08-06 20:12:02 -0400

    sortingQuestions.js started

---
+++ react-sorting-hat/src/sortingQuestions.js
@@ -0,0 +1,20 @@
+export const Questions = 
+[
+    {
+        id: 0,
+        questionText: "What do you think is most important?",
+        answer1: {answerText:"Bravery", pointArr: [4, 1, 0, 2]},
+        answer2: {answerText:"Hard Work", pointArr: [1, 2, 4, 2]},
+        answer3: {answerText:"Wisdom", pointArr: [2, 4, 1, 2]},
+        answer4: {answerText:"Achievement", pointArr: [2, 1, 3, 4]}
+    },
+    {
+        id: 1,
+        questionText: "How important is one's ancestry?",
+        answer1: {answerText:"It's something to consider", pointArr: [1, 3, 3, 2]},
+        answer2: {answerText:"Not at all", pointArr: [4, 2, 2, 0]},
+        answer3: {answerText:"You doubt my blood?!", pointArr: [0, 0, 2, 4]},
+        answer4: {answerText:"Fairly", pointArr: [1, 1, 2, 2]}
+    },
+
+]
