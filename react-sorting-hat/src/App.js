813df5057f72943cbfb5e27b9b7aed0df3eb4dfa
thomas <t_huynh927@yahoo.com>
2020-03-11 20:47:47 -0700

    first commit, almost done

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -2,25 +2,70 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Question from "./components/Question";
 
-export default App;
+const questions = [
+  {
+    question: "Which adjective best describes you?",
+    brave: "g",
+    patient: "h",
+    witty: "r",
+    ambitious: "s"
+  },
+  {
+    question: "What is your spirit animal?",
+    snake: "s",
+    raven: "r",
+    badger: "h",
+    lion: "g"
+  },
+  {
+    question: "What's your favorite color?",
+    yellow: "h",
+    green: "s",
+    red: "g",
+    blue: "r"
+  },
+  {
+    question: "Choose your element.",
+    air: "r",
+    fire: "g",
+    water: "s",
+    earth: "h"
+  }
+]
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tally: {
+        g: 0,
+        h: 0,
+        r: 0,
+        s: 0
+      },
+      currentQuestion: false
+    }
+  }
+
+  handleAnswerQuestion = (ans) => {
+    const prevTally = this.state.tally
+    const currentQuestion = this.state.currentQuestion < 4 ? this.state.currentQuestion + 1 : false
+    this.setState({ tally: {...prevTally, [ans]: prevTally[ans] + 1}, currentQuestion })
+  }
+
+  renderQuestions  = () => {
+    return <Question handleAnswerQuestion={this.handleAnswerQuestion} question={questions[this.state.currentQuestion - 1]} />
+  }
+  
+  render() {
+    console.log(this.state.tally)
+    return (
+      <div>
+        {this.state.currentQuestion && this.renderQuestions()}
+        {!this.state.currentQuestion && <button onClick={() => this.setState({ currentQuestion: 1 })}>Start!</button>}
+      </div>
+    )
+  }
+}
---
+++ react-sorting-hat/src/components/Question.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Question = ({ question, handleAnswerQuestion }) => {
+    const questionKeys = Object.keys(question).slice(1,5)
+    console.log(questionKeys)
+    console.log(question)
+
+    const answerQuestion = (ans) => {
+        handleAnswerQuestion(question[questionKeys[ans]])
+    }
+    return (
+        <div>
+            <h2>{question.question}</h2>
+            <div style={{ display: "flex", flexWrap: "wrap"  }}>
+                <ul>
+                    <li onClick={() => answerQuestion(0)}>{questionKeys[0]}</li>
+                    <li onClick={() => answerQuestion(1)}>{questionKeys[1]}</li>
+                </ul>
+                <ul>
+                    <li onClick={() => answerQuestion(2)}>{questionKeys[2]}</li>
+                    <li onClick={() => answerQuestion(3)}>{questionKeys[3]}</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Question;
